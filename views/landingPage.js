48a41a3710474bc6ab2ee32cdf4a580b2e26e9c3
f5m0b <h.sun@alumni.ubc.ca>
2019-11-15 12:13:14 -0800

    Ajax implemented

--- views/landingPage.js
+++ views/landingPage.js
@@ -1,6 +1,30 @@
 // this is where the code that will take the time frame given by the user and pass on the information
 // to the back end where the http requests will be made
 
-$('#datepicker').datepicker({
-    uiLibrary: 'bootstrap'
+$(document).ready(function() {
+    $('#submit').click(function() {
+        var repoUrl = $('#repoUrl').val();
+        var monthYear = $('#monthYear').val();
+        if (repoUrl === "" || monthYear === "") {
+            alert("please fill out fields");
+            return false;
+        }
+        //alert( "repoUrl: " + repoUrl + " monthYear: " + monthYear );
+        $.ajax({
+            url: "/scripts/example.py",
+            dataType: 'json',
+            data: JSON.stringify({"repoUrl": repoUrl, "monthYear": monthYear}),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        })
+      });
 });
+
+$('#submit').click(function() {
+    alert( "Handler for .click() called." );
+  });
