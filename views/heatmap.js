0259a3760e40b11e6a98c82c92c4e862f4621f33
joycheng <joycheng@alumni.ubc.ca>
2019-11-17 17:21:05 -0800

    updated x and y axis, still need to fix up dynamic gridsize

--- views/heatmap.js
+++ views/heatmap.js
@@ -4,15 +4,17 @@
 //import * as d3 from '../lib/d3.v3.min';
 
 var margin = { top: 50, right: 0, bottom: 100, left: 40 },
+    developers = ["Dev1", "Dev2", "Dev3", "Dev4", "Dev5", "Dev6", "Dev7"],
     width = 1060 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
-    gridSize = Math.floor(width / 24),
-    legendElementWidth = gridSize*2,
+    height = developers.length * 100 - margin.top - margin.bottom,
+    gridSize = Math.floor(width / 6),
+    legendElementWidth = 88,
     buckets = 9,
     colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
-    developers = ["Dev1", "Dev2", "Dev3", "Dev4", "Dev5", "Dev6", "Dev7"],
-    periods = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
-    datasets = ["data.json", "data2.tsv"];
+    periods = ['\u2264'+"1day", '\u2264'+"7days", '\u2264'+"30days", '\u2264'+"90days", '\u2264'+"180days", '\u2265'+"180days"];
+    datasets = ["data.json", "data2.json"];
+
+var gridSizeY = Math.floor(height / developers.length);
 
 var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -25,9 +27,9 @@ var dayLabels = svg.selectAll(".developerLabel")
     .enter().append("text")
     .text(function (d) { return d; })
     .attr("x", 0)
-    .attr("y", function (d, i) { return i * gridSize; })
+    .attr("y", function (d, i) { return i * gridSizeY; })
     .style("text-anchor", "end")
-    .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+    .attr("transform", "translate(-6," + gridSizeY / 1.5 + ")")
     .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
 
 var timeLabels = svg.selectAll(".periodLabel")
@@ -46,38 +48,38 @@ var heatmapChart = function(data) {
 
         var colorScale = d3.scale.quantile()
             .domain([0, buckets - 1, Math.max.apply(Math, data.map(function (d) {
-                return d.value;
+                return d.Value;
             }))])
             .range(colors);
 
-        var cards = svg.selectAll(".hour")
+        var cards = svg.selectAll(".Period")
             .data(data, function (d) {
-                return d.day + ':' + d.hour;
+                return d.Developer + ':' + d.Period;
             });
 
         cards.append("title");
 
         cards.enter().append("rect")
             .attr("x", function (d) {
-                return (d.hour - 1) * gridSize;
+                return (d.Period - 1) * gridSize;
             })
             .attr("y", function (d) {
-                return (d.day - 1) * gridSize;
+                return (d.Developer - 1) * gridSizeY;
             })
             .attr("rx", 4)
             .attr("ry", 4)
-            .attr("class", "hour bordered")
+            .attr("class", "Period bordered")
             .attr("width", gridSize)
-            .attr("height", gridSize)
+            .attr("height", gridSizeY)
             .style("fill", colors[0]);
 
         cards.transition().duration(1000)
             .style("fill", function (d) {
-                return colorScale(d.value);
+                return colorScale(d.Value);
             });
 
         cards.select("title").text(function (d) {
-            return d.value;
+            return d.Value;
         });
 
         cards.exit().remove();
@@ -96,7 +98,7 @@ var heatmapChart = function(data) {
             })
             .attr("y", height)
             .attr("width", legendElementWidth)
-            .attr("height", gridSize / 2)
+            .attr("height", 20)
             .style("fill", function (d, i) {
                 return colors[i];
             });
